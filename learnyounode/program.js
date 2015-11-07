90728f2db5e484cc6786a5b0861ec9f888ab6679
Jinpeng LV <gepo.lv@hotmail.com>
2015-11-07 12:41:39 -0800

    add learnyounode code and .gitignore file

---
+++ learnyounode/program.js
@@ -0,0 +1,28 @@
+var p = require("./learn")
+
+//p.p1()
+
+//p.p2(process.argv)
+
+//p.p3(process.argv[2]);
+
+//p.p4(process.argv[2]);
+
+//p.p5(process.argv);
+
+/*p6 is the same as p5 */
+//p.p6(process.argv);
+
+//p.p7(process.argv[2]);
+
+//p.p8(process.argv[2]);
+
+//p.p9(process.argv)
+
+//p.p10(process.argv[2]);
+
+//p.p11(process.argv[2], process.argv[3]);
+
+//p.p12(process.argv[2]);
+
+p.p13(process.argv[2]);
